00fb239b033340dd0cc18b5224c22b8eed37552c
DRIOUICHE ACHRAF <as.driouiche@gmail.com>
2024-11-11 11:48:10 +0100

    Add Files

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path');
+var bodyParser = require('body-parser')
+
+
+const app = express()
+
+
+
+app.get('/user/:id', function (request, response) {
+     response.send('user ' + request.params.id)
+})
+
+
+app.post('/sendResult', (req,res) => {
+     console.clear()
+     console.log(req.body)
+     res.send("You can continue")
+   
+  
+})
+
+
+app.listen(8080,(req,res) => {
+     console.log("server started at PORT : 8080 | ASLOO")
+})
